8ed6092be742b3f190065d81c1896c78cb1c1df4
maximilianuos <90860393+maximilianuos@users.noreply.github.com>
2023-07-23 20:33:49 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xce3582da019d1ceb5c07a29385602e84a1dc926c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
